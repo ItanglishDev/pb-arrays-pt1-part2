502e0c9b6030a4de0ea8fc91e6aa41057e7cc4b5
chris <chrishethertonweb@gmail.com>
2022-07-07 13:40:39 +0200

    exercise 1 done

---
+++ solutions.js
@@ -0,0 +1,14 @@
+const myLuckyNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const resultIs =
+  myLuckyNumber[0] +
+  myLuckyNumber[1] +
+  myLuckyNumber[2] +
+  myLuckyNumber[3] +
+  myLuckyNumber[4] +
+  myLuckyNumber[5] +
+  myLuckyNumber[6] +
+  myLuckyNumber[7] +
+  myLuckyNumber[8] +
+  myLuckyNumber[9];
+
+console.log(resultIs);
